03bbfd4e8e52a5e140e7a0f5e894ce53091d1be8
Aditya Mathur <aditya94mathur@gmail.com>
2025-08-04 13:15:38 +0530

    Delete next.config.js

--- next.config.js
+++
@@ -1,9 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-  images: {
-    domains: ['yourdomain.com'], // Optional, or remove if not using remote images
-  },
-}
-
-module.exports = nextConfig
